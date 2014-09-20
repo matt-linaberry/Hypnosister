3c0c742b05f443bda744d34641345809cb67c9f8
Matt Linaberry <matt.linaberry@we-ics.com>
2014-09-19 23:49:43 -0400

    Hypnosister from the BNR book.

---
+++ BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Matthew Linaberry on 8/30/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
---
+++ Hypnosister/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Hypnosister
+//
+//  Created by Matthew Linaberry on 8/30/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
